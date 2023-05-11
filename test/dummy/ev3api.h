5857a98b57771073a29c73e0ee874389ef181c78
KakinokiKanta <kanta12621@icloud.com>
2023-05-11 18:43:07 +0900

    add:必要なAPIのダミーを追加

---
+++ test/dummy/ev3api.h
@@ -0,0 +1,10 @@
+/**
+ * @file ev3api.h
+ * @brief Cのヘッダファイルのインクルード（ダミー）
+ * @author KakinokiKanta
+ */
+#pragma once
+
+#include "ev3api_motor.h"
+#include "ev3api_button.h"
+#include "ev3api_voltage.h"
