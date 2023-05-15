bf3daa05276f19fea501a5520930063a672d2510
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2023-05-15 20:41:52 +0900

    add: Controller.cpp Controller.h

---
+++ module/API/Controller.cpp
@@ -0,0 +1,56 @@
+/**
+ * @file Controller.cpp
+ * @brief モーター制御に用いる関数をまとめたラッパークラス
+ * @author aridome222
+ */
+#include "Controller.h"
+
+Controller::Controller() : rightWheel(PORT_B), leftWheel(PORT_C), armMotor(PORT_A) {}
+
+int Controller::limitPwmValue(const int value)
+{
+  if(value > MOTOR_PWM_MAX) {
+    return MOTOR_PWM_MAX;
+  } else if(value < MOTOR_PWM_MIN) {
+    return MOTOR_PWM_MIN;
+  }
+  return value;
+}
+
+// 右モータにPWM値をセット
+void Controller::setRightMotorPwm(const int pwm)
+{
+  rightWheel.setPWM(limitPwmValue(pwm));
+}
+
+// 左モータにPWM値をセット
+void Controller::setLeftMotorPwm(const int pwm)
+{
+  leftWheel.setPWM(limitPwmValue(pwm));
+}
+
+// タイヤのモータを停止する
+void Controller::stopMotor()
+{
+  leftWheel.stop();
+  rightWheel.stop();
+}
+
+// アームのモータにPWM値をセット
+void Controller::setArmMotorPwm(const int pwm)
+{
+  armMotor.setPWM(limitPwmValue(pwm));
+}
+
+// アームのモータを停止する
+void Controller::stopArmMotor()
+{
+  armMotor.stop();
+}
+
+// 自タスクスリープ（デフォルトは10ミリ秒）
+void Controller::sleep(int milliSec)
+{
+  // clock.sleep()はマイクロ秒指定なので，単位を合わせて呼び出す
+  clock.sleep(milliSec * 1000);
+}
---
+++ module/API/Controller.h
@@ -0,0 +1,71 @@
+/**
+ * @file Controller.h
+ * @brief モーター制御に用いる関数をまとめたラッパークラス
+ * @author aridome222
+ */
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include "Measurer.h"
+#include "ev3api.h"
+#include "Motor.h"
+#include "Clock.h"
+
+class Controller {
+ public:
+  static const int MOTOR_PWM_MAX = 100;
+  static const int MOTOR_PWM_MIN = -100;
+
+  Controller();
+
+  /**
+   * タイヤのモータにPWM値をセット
+   * @param pwm PWM値
+   */
+  void setRightMotorPwm(const int pwm);
+  void setLeftMotorPwm(const int pwm);
+
+  /**
+   * タイヤのモータを停止する
+   */
+  void stopMotor();
+
+  /**
+   * アームのモータにPWM値をセット
+   * @param pwm PWM値
+   */
+  void setArmMotorPwm(const int pwm);
+
+  /**
+   * アームのモータを停止する
+   */
+  void stopArmMotor();
+
+  /**
+   * 自タスクスリープ（デフォルトは10ミリ秒）
+   * @param milliSec スリープ時間(ミリ秒)
+   */
+  void sleep(int milliSec = 10);
+
+  // /**
+  //  * シミュレータへ競技の終了を通知する
+  //  */
+  // void notifyCompletedToSimulator();
+
+ private:
+  ev3api::Motor rightWheel;
+  ev3api::Motor leftWheel;
+  ev3api::Motor armMotor;
+  ev3api::Clock clock;
+
+  Measurer measurer;
+
+  /**
+   * モータに設定するPWM値の制限
+   * @param value 入力されたPWM値
+   * @return 制限されたPWM値
+   */
+  int limitPwmValue(const int value);
+};
+
+#endif
