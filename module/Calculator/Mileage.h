1960b3f6846d30f7cdf0a52ad98dae22bc9833af
desty505 <ydogmai@outlook.jp>
2023-05-22 10:17:58 +0900

    fix: 不必要な部分の削除

--- module/Calculator/Mileage.h
+++ module/Calculator/Mileage.h
@@ -1,19 +1,7 @@
 /**
  **  @file Mileage.h
  **  @brief 走行距離を計算するクラス
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
  **  @author desty505
-=======
- **  @author TanakaShinnosuke
->>>>>>> work-4
-=======
- **  @author TanakaShinnosuke
->>>>>>> work-4
-=======
- **  @author TanakaShinnosuke
->>>>>>> work-4
  **/
 
 #ifndef MILEAGE_H
@@ -24,10 +12,6 @@
 class Mileage {
  public:
   /**
-<<<<<<< HEAD
-=======
-  ** @fn double calculateWheelMileage(int angle);
->>>>>>> work-4
   ** @brief タイヤの累計走行距離を計算する
   ** @param angle タイヤの回転角度[deg]
   ** @return タイヤの累計走行距離[mm]
@@ -35,10 +19,6 @@ class Mileage {
   static double calculateWheelMileage(int angle);
 
   /**
-<<<<<<< HEAD
-=======
-   ** @fn　double calculateMileage(int rightAngle, int leftAngle);
->>>>>>> work-4
    ** @brief 走行体全体の累計走行距離を計算する
    ** @param rightAngle 右タイヤの回転角度[deg]
    ** @param leftAngle 左タイヤの回転角度[deg]
