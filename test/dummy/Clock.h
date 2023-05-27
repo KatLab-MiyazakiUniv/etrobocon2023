6902ae0d3fa26684637633417d5fe0e6a9c795fc
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2023-05-27 14:49:59 +0900

    add: dummyにClock.hを追加

---
+++ test/dummy/Clock.h
@@ -0,0 +1,16 @@
+/**
+ * @file Clock.h
+ * @brief クロッククラス（ダミー）
+ * @author YKhm20020 aridome222
+ */
+#ifndef CLOCK_H
+#define CLOCK_H
+
+namespace ev3api {
+  class Clock {
+   public:
+    void sleep(int duration){};
+  };
+}  // namespace ev3api
+
+#endif
