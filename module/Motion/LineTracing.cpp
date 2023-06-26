f12c6bd77e7a4e65dba3de9c73108cdc30d14515
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2023-06-26 16:47:43 +0900

    add: ライントレースの中間クラスを追加

---
+++ module/Motion/LineTracing.cpp
@@ -0,0 +1,91 @@
+/**
+ * @file   LineTracing.cpp
+ * @brief  ライントレース動作の中間クラス
+ * @author YKhm20020
+ */
+
+#include "LineTracing.h"
+using namespace std;
+
+LineTracing::LineTracing(double _targetDistance, int _targetBrightness, int _pwm, const PidGain& _gain, bool& _isLeftEdge)
+  : targetDistance(_targetDistance),
+    targetBrightness(_targetBrightness),
+    pwm(_pwm),
+    gain(_gain),
+    isLeftEdge(_isLeftEdge)
+{
+}
+
+void LineTracing::run()
+{
+  int currentPid = 0;
+  int sign = 0;
+  Pid pid(gain.kp, gain.ki, gain.kd, targetBrightness);
+
+  // 事前条件を判定する
+  if(!isMetPrecondition(pwm, targetDistance)) {
+    return;
+  }
+
+  // 左右で符号を変える
+  sign = isLeftEdge ? -1 : 1;
+
+  // 呼び出し時の走行距離
+  initLeftMileage = Mileage::calculateWheelMileage(Measurer::getLeftCount());
+  initRightMileage = Mileage::calculateWheelMileage(Measurer::getRightCount());
+
+  // 継続条件を満たしている間ループ
+  while(isMetPostcondition(initLeftMileage, initRightMileage)) {
+    // PID計算
+    currentPid = pid.calculatePid(Measurer::getBrightness()) * sign;
+
+    // モータのPWM値をセット（0を超えないようにセット）
+    int rightPwm = pwm > 0 ? max(pwm - (int)currentPid, 0) : min(pwm + (int)currentPid, 0);
+    int leftPwm = pwm > 0 ? max(pwm + (int)currentPid, 0) : min(pwm - (int)currentPid, 0);
+    Controller::setRightMotorPwm(rightPwm);
+    Controller::setLeftMotorPwm(leftPwm);
+  }
+
+  // モータの停止
+  Controller::stopMotor();
+}
+
+bool LineTracing::isMetPrecondition(int pwm, double targetDistance)
+{
+  const int BUF_SIZE = 256;
+  char buf[BUF_SIZE];
+
+  // pwm値が0の場合はwarningを出して終了する
+  if(pwm == 0) {
+    logger.logWarning("The pwm value passed to LineTracing is 0");
+    return false;
+  }
+
+  // 目標距離が0以下の場合はwarningを出して終了する
+  if(targetDistance <= 0) {
+    snprintf(buf, BUF_SIZE, "The targetDistance value passed to LineTracing is %.2f",
+             targetDistance);
+    logger.logWarning(buf);
+    return false;
+  }
+
+  return true;
+}
+
+bool LineTracing::isMetPostcondition(double initLeftMileage, double initRightMileage){
+  return false;
+}
+
+void LineTracing::logRunning()
+{
+  const int BUF_SIZE = 256;
+  char buf[BUF_SIZE];  // log用にメッセージを一時保持する領域
+  const char* str = isLeftEdge ? "true" : "false";
+
+  snprintf(buf, BUF_SIZE,
+           "Run LineTracing (targetDistance: %.2f, targetBrightness: %d, pwm: %d, gain: "
+           "(%.2f,%.2f,%.2f), isLeftEdge: %s)",
+           targetDistance, targetBrightness, pwm, gain.kp, gain.ki, gain.kd,
+           str);
+  logger.log(buf);
+}
---
+++ module/Motion/LineTracing.h
@@ -0,0 +1,62 @@
+/**
+ * @file   LineTracing.h
+ * @brief  ライントレース動作
+ * @author YKhm20020
+ */
+
+#ifndef LINE_TRACING_H
+#define LINE_TRACING_H
+
+#include "Motion.h"
+#include "Mileage.h"
+#include "Pid.h"
+
+class LineTracing : public Motion {
+ public:
+  /**
+   * コンストラクタ
+   * @param _targetDistance 目標距離 0~
+   * @param _targetBrightness 目標輝度 0~
+   * @param _pwm PWM値 -100~100
+   * @param _gain PIDゲイン
+   * @param _isLeftEdge エッジの左右判定(true:左エッジ, false:右エッジ)
+   */
+  LineTracing(double _targetDistance, int _targetBrightness, int _pwm, const PidGain& _gain, bool& _isLeftEdge);
+
+  /**
+   * @brief ライントレースする
+   */
+  void run();
+
+  /**
+   * @brief ライントレースする際の事前条件判定をする
+   * @param pwm PWM値
+   * @param targetDistance 目標距離
+   * @note オーバーライド必須
+   */
+  virtual bool isMetPrecondition(int pwm, double targetDistance) = 0;
+
+  /**
+   * @brief ライントレースする際の継続条件判定をする　返り値がfalseでモーターが止まる
+   * @param initLeftMileage   ライントレースを始めた時点での左車輪の走行距離
+   * @param initRightMileage  ライントレースを始めた時点での右車輪の走行距離
+   * @note オーバーライド必須
+   */
+  virtual bool isMetPostcondition(double initLeftMileage, double initRightMileage) = 0;
+
+  /**
+   * @brief 実行のログを取る
+   */
+  void logRunning();
+
+  private:
+    double targetDistance;// 目標距離 0~
+    int targetBrightness; // 目標輝度 0~
+    int pwm;  // PWM値 -100~100
+    PidGain gain;  // PIDゲイン
+    bool& isLeftEdge;  // エッジの左右判定(true:左エッジ, false:右エッジ)
+    double initLeftMileage;   // クラス呼び出し時の左車輪の走行距離
+    double initRightMileage;  // クラス呼び出し時の右車輪の走行距離
+};
+
+#endif
