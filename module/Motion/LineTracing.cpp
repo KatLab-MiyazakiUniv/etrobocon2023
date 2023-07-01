a5ce42e94adea341a0391b1289670fbec84539f0
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2023-07-01 17:54:10 +0900

    fix: 子クラス実装に合わせてファイルを修正

--- module/Motion/LineTracing.cpp
+++ module/Motion/LineTracing.cpp
@@ -7,11 +7,10 @@
 #include "LineTracing.h"
 using namespace std;
 
-LineTracing::LineTracing(double _targetSpeed, int _targetBrightness, int _pwm, const PidGain& _gain,
+LineTracing::LineTracing(double _targetSpeed, int _targetBrightness, const PidGain& _gain,
                          bool& _isLeftEdge)
   : targetSpeed(_targetSpeed),
     targetBrightness(_targetBrightness),
-    pwm(_pwm),
     gain(_gain),
     isLeftEdge(_isLeftEdge)
 {
@@ -25,7 +24,7 @@ void LineTracing::run()
   Pid pid(gain.kp, gain.ki, gain.kd, targetBrightness);
 
   // 事前条件を判定する
-  if(!isMetPrecondition(pwm, targetSpeed)) {
+  if(!isMetPrecondition(basePwm, targetSpeed)) {
     return;
   }
 
@@ -38,11 +37,10 @@ void LineTracing::run()
 
   // 継続条件を満たしている間ループ
   while(isMetPostcondition()) {
-    // 目標速度に対するpwm値を計算
+    // 初期pwm値を計算
     SpeedCalculator SpeedCalculator(targetSpeed);
     basePwm = SpeedCalculator.calcPwmFromSpeed();
 
-    // PIDで旋回値を計算
     // 目標速度に対するpwm値を計算
     turnPwm = pid.calculatePid(Measurer::getBrightness()) * edgeSign;
 
@@ -51,10 +49,34 @@ void LineTracing::run()
     int leftPwm = basePwm > 0 ? max(basePwm + (int)turnPwm, 0) : min(basePwm - (int)turnPwm, 0);
     Controller::setRightMotorPwm(rightPwm);
     Controller::setLeftMotorPwm(leftPwm);
+
+    // 10ミリ秒待機
+    timer.sleep(10);
   }
 
   // モータの停止
   Controller::stopMotor();
+}
+
+bool LineTracing::isMetPrecondition(int basePwm, double targetSpeed)
+{
+  const int BUF_SIZE = 256;
+  char buf[BUF_SIZE];
+
+  // 初期pwm値が0の場合はwarningを出して終了する
+  if(basePwm == 0) {
+    logger.logWarning("The basePwm value passed to LineTracing is 0");
+    return false;
+  }
+
+  // targetSpeed値が0以下の場合はwarningを出して終了する
+  if(targetSpeed <= 0) {
+    snprintf(buf, BUF_SIZE, "The targetSpeed value passed to LineTracing is %lf", targetSpeed);
+    logger.logWarning(buf);
+    return false;
+  }
+
+  return true;
 }
 
 void LineTracing::logRunning()
@@ -63,9 +85,11 @@ void LineTracing::logRunning()
   char buf[BUF_SIZE];  // log用にメッセージを一時保持する領域
   const char* str = isLeftEdge ? "true" : "false";
 
+  // targetValueと%~のオーバーライド必須
   snprintf(buf, BUF_SIZE,
-           "Run LineTracing (targetSpeed: %.2f, targetBrightness: %d, pwm: %d, gain: "
+           "Run \"targetValue\" LineTracing (\"targetValue\": , targetSpeed: %.2f, "
+           "targetBrightness: %d, basePwm: %d, gain: "
            "(%.2f,%.2f,%.2f), isLeftEdge: %s)",
-           targetSpeed, targetBrightness, pwm, gain.kp, gain.ki, gain.kd, str);
+           targetSpeed, targetBrightness, basePwm, gain.kp, gain.ki, gain.kd, str);
   logger.log(buf);
 }
--- module/Motion/LineTracing.h
+++ module/Motion/LineTracing.h
@@ -18,12 +18,10 @@ class LineTracing : public Motion {
    * コンストラクタ
    * @param _targetSpeed 目標速度 0~
    * @param _targetBrightness 目標輝度 0~
-   * @param _pwm PWM値 -100~100
    * @param _gain PIDゲイン
    * @param _isLeftEdge エッジの左右判定(true:左エッジ, false:右エッジ)
    */
-  LineTracing(double _targetSpeed, int _targetBrightness, int _pwm, const PidGain& _gain,
-              bool& _isLeftEdge);
+  LineTracing(double _targetSpeed, int _targetBrightness, const PidGain& _gain, bool& _isLeftEdge);
 
   /**
    * @brief ライントレースする
@@ -32,11 +30,11 @@ class LineTracing : public Motion {
 
   /**
    * @brief ライントレースする際の事前条件判定をする
-   * @param pwm PWM値
+   * @param basePwm 初期PWM値
    * @param targetSpeed 目標速度
    * @note オーバーライド必須
    */
-  virtual bool isMetPrecondition(int pwm, double targetSpeed) = 0;
+  bool isMetPrecondition(int basePwm, double targetSpeed);
 
   /**
    * @brief ライントレースする際の継続条件判定をする　返り値がfalseでモーターが止まる
@@ -50,10 +48,11 @@ class LineTracing : public Motion {
   void logRunning();
 
  private:
-  double targetSpeed;       // 目標速度 0~
-  int targetBrightness;     // 目標輝度 0~
-  int pwm;                  // PWM値 -100~100
-  PidGain gain;             // PIDゲイン
+  double targetSpeed;    // 目標速度 0~
+  int targetBrightness;  // 目標輝度 0~
+  int basePwm;           // 初期PWM値 -100~100
+  PidGain gain;          // PIDゲイン
+  class Timer timer;
   bool& isLeftEdge;         // エッジの左右判定(true:左エッジ, false:右エッジ)
   double initLeftMileage;   // クラス呼び出し時の左車輪の走行距離
   double initRightMileage;  // クラス呼び出し時の右車輪の走行距離
