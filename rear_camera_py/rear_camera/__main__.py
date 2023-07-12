914300be19e8ccf82b6a3a69585c40dde8fdf693
kawanoichi <kawakawaichi1001@gmail.com>
2023-07-12 20:45:12 +0900

    update:環境構築

---
+++ rear_camera_py/rear_camera/__main__.py
@@ -0,0 +1,27 @@
+"""mainモジュール.
+
+プロジェクトルートで、以下のコマンドを実行すると最初に呼び出されるファイル
+> python -m rear_camera
+
+必要最低限のコードのみを記述するようにする
+@author: kawanoichi
+"""
+
+
+import argparse
+from rear_camera.camera_interface import CameraInterface
+
+
+def main():
+    """リアカメラシステム."""
+    parser = argparse.ArgumentParser(description="リアカメラに関するプログラム")
+    parser.add_argument("--camera-num", type=int, default=0,
+                        help="カメラID")
+    args = parser.parse_args()
+
+    camera = CameraInterface(args.camera_num)
+    camera.start_camera()
+
+
+if __name__ == "__main__":
+    main()
