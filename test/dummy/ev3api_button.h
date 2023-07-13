6b2b8441570f741cf9da6e6785c9a1022c3635ef
KakinokiKanta <kanta12621@icloud.com>
2023-07-13 16:45:36 +0900

    update: 中央ボタンを使わない仕様に変更

--- test/dummy/ev3api_button.h
+++ test/dummy/ev3api_button.h
@@ -18,7 +18,7 @@ typedef enum {
   RIGHT_BUTTON = 1,  // 右ボタン
   UP_BUTTON = 2,     // Spikeでは使えないけど一応
   DOWN_BUTTON = 3,   // Spikeでは使えないけど一応
-  ENTER_BUTTON = 4,  // 中央ボタン
+  ENTER_BUTTON = 4,  // 中央ボタン(今年の環境では使用できない)
   BACK_BUTTON = 5,   // Spikeでは使えないけど一応
   TNUM_BUTTON = 6    // Spikeでは使えないけど一応
 } button_t;
