23fa67c48fb6a6789a5c4519d9f25be74cd2069b
miyashita64 <javajavawookjavawook@gmail.com>
2023-07-15 12:13:38 +0100

    update:画像取得成功

--- rear_camera_py/src/__main__.py
+++ rear_camera_py/src/__main__.py
@@ -11,6 +11,7 @@
 import argparse
 from camera_interface import CameraInterface
 from datetime import datetime
+from PIL import Image
 
 def main():
     """リアカメラシステム."""
@@ -25,9 +26,14 @@ def main():
     camera.start_camera()
     # 画像を取得
     img = camera.capture_image()
-    img.save(now_time)
+
+    # カラーチャンネルの順序を変換（BGRからRGB）
+    image_data_rgb = img[:, :, ::-1]
+
+    pil_img = Image.fromarray(image_data_rgb)
+    pil_img.save(now_time+".png")
 
 
 if __name__ == "__main__":
     print("Rear Camera Start!!")
-    # main()
+    main()
--- rear_camera_py/src/zikken.py
+++ rear_camera_py/src/zikken.py
@@ -21,34 +21,49 @@ def test1():
         (b, ), meta = pickle.load(tf)
 
     img = h.make_image(b, capture_config['main'])
-    h.save(img, meta, 'sample.png')
+    h.save(img, meta, 'zikken.png')
 
-def test2():
-    """画像
-    https://raspida.com/cameramodule-v3-1st-impression
+# def test2():
+#     """画像
+#     https://raspida.com/cameramodule-v3-1st-impression
+#     """
+#     picam2 = Picamera2()
+#     camera_config = picam2.create_preview_configuration()
+#     picam2.configure(camera_config)
+#     picam2.start_preview(Preview.QTGL)
+#     picam2.start()
+#     time.sleep(2)
+#     picam2.capture_file("test.jpg")
+
+# def test3():
+#     """動画
+#     https://dev.classmethod.jp/articles/raspberrypi-camera-freenove/
+#     """
+#     picam2 = Picamera2()
+#     video_config = picam2.create_video_configuration()
+#     picam2.configure(video_config)
+#     encoder = H264Encoder(bitrate=10000000)
+#     output = "test.h264"
+#     picam2.start_recording(encoder, output)
+#     time.sleep(10)
+#     picam2.stop_recording()
+
+def test4():
     """
-    picam2 = Picamera2()
-    camera_config = picam2.create_preview_configuration()
-    picam2.configure(camera_config)
-    picam2.start_preview(Preview.QTGL)
-    picam2.start()
+    OK
+    """
+    camera = Picamera2()
+    config_capture = camera.create_still_configuration()
+    camera.configure(config_capture)
+    camera.start()
     time.sleep(2)
-    picam2.capture_file("test.jpg")
 
-def test3():
-    """動画
-    https://dev.classmethod.jp/articles/raspberrypi-camera-freenove/
-    """
-    picam2 = Picamera2()
-    video_config = picam2.create_video_configuration()
-    picam2.configure(video_config)
-    encoder = H264Encoder(bitrate=10000000)
-    output = "test.h264"
-    picam2.start_recording(encoder, output)
-    time.sleep(10)
-    picam2.stop_recording()
+    camera.capture_file("zikken_test4.png")
+
+
 
 if __name__ == "__main__":
-    test1()
-    test2()
-    test3()
+    # test1()
+    # test2()
+    # test3()
+    test4()
