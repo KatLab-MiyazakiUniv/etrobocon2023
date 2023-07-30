6f489a1927e253cf397c481ad2a040eee668a46b
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2023-07-31 07:22:44 +0900

    CorrectionRotationをRotationから作る前

---
+++ module/Motion/CorrectingRotation.h
@@ -0,0 +1,40 @@
+/**
+ * @file   CorrectingRotation.h
+ * @brief  角度補正回頭
+ * @author aridome222
+ */
+
+#ifndef CORRECTING_ROTATION_H
+#define CORRECTING_ROTATION_H
+
+#include "Motion.h"
+#include "Mileage.h"
+#include "StringOperator.h"
+#include "SystemInfo.h"
+
+class CorrectingRotation : public Motion {
+ public:
+  /**
+   * コンストラクタ
+   * @param _targetAngle 目標角度(deg) 0~89
+   * @param _targetSpeed 目標速度[mm/s] 0~
+   */
+  CorrectingRotation(int _targetAngle, double _targetSpeed);
+
+  /**
+   * @brief 角度補正回頭する
+   */
+  void run();
+
+  /**
+   * @brief 実行のログを取る
+   */
+  void logRunning();
+
+ private:
+  static constexpr int NO_CORRECTION_ANGLE = 2;  // 補正免除角度(deg)
+  int targetAngle;                               // 目標角度(deg) 0~89
+  double targetSpeed;                               // 目標速度[mm/s] 0~
+};
+
+#endif
