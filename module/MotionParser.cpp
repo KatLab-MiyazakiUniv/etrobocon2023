1d1009457c786c5c023e0a4cf8ce56d3934afee1
miyashita64 <javajavawookjavawook@gmail.com>
2023-08-12 12:12:07 +0100

    refactor: clang-formatを適用

--- module/MotionParser.cpp
+++ module/MotionParser.cpp
@@ -117,8 +117,8 @@ vector<Motion*> MotionParser::createMotions(const char* commandFilePath, int tar
       CorrectingRotation* xr = new CorrectingRotation(atoi(params[1]),   // 目標角度
                                                       atof(params[2]));  // 目標速度
 
-      motionList.push_back(xr);  // 動作リストに追加
-    } else if(command == COMMAND::IS) {                              // 交点内移動（直進）
+      motionList.push_back(xr);                                    // 動作リストに追加
+    } else if(command == COMMAND::IS) {                            // 交点内移動（直進）
       InCrossStraight* is = new InCrossStraight(atof(params[1]),   // 目標距離
                                                 atof(params[2]));  // 目標速度 [mm/s]
 
