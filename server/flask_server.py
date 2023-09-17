f1e3bffc21076621cd0f509884e40d572dc65ea7
miyashita64 <javajavawookjavawook@gmail.com>
2023-09-17 11:01:57 +0900

    update: 撮影終了フラグをファイルとして管理するように変更

--- server/flask_server.py
+++ server/flask_server.py
@@ -1,130 +1,91 @@
-'''
- RasPiにWebサーバを設置する 
- @auther  desty505 aridome222
+'''ロボットの走行情報を提供するWebサーバ.
+
+ @auther  desty505 aridome222 miyashita64
 '''
 import os
 import csv
 import re
+import pathlib
 from flask import Flask, request
 
-
-# サーバが扱うファイルの設定
-FILE_PATH = "datafiles/data.csv"
-
-# サーバが扱うロボットの情報
-# state
-#   - notReady: 通信未確立
-#   - wait: 開始合図待ち
-#   - start: 走行開始
-#   - lap: LAPゲート通過
-#   - finish: 処理停止
+# ロボットの走行情報
 robot_info = {
+    # state
+    #   - notReady: 通信未確立
+    #   - wait: 開始合図待ち
+    #   - start: 走行開始
+    #   - lap: LAPゲート通過
+    #   - finish: 処理停止
+    # cameraActionSkipFlag
+    #   - True: 撮影動作をスキップする
+    #   - False: 撮影動作を行う
     "state": "notReady"
 }
+
+# 撮影終了フラグ用ファイルのパス
+SKIP_FLAG_FILE = "skip_camera_action.flag"
 
 app = Flask(__name__)
 
 
-# '/'へのGETリクエストに対する操作
-@app.route('/', methods=["GET"])
-def read() -> str:
-    """GETリクエストに対しcsvファイルの内容を返す.
-    "http://サーバIPアドレス:8000/"にGETリクエストされたときに実行される
-    
-    Returns:
-        str:  サーバが持つcsvファイルの内容
-    """
-
-    # データを取得
-    file = open(FILE_PATH)
-    open_data = file.read()
-    file.close()
-
-    # 値の送信
-    return open_data
-
-
-# '/'へのPOSTリクエストに対する操作
-@app.route('/', methods=["POST"])
-def write() -> str:
-    """POSTリクエストに対しcsvファイルの内容を更新して、返す.
-    "http://サーバIPアドレス:8000/"にPOSTリクエストされたときに実行される
-    
-    Returns:
-        str:  サーバが持つcsvファイルの内容
-    """
-
-    # request.get_data()を使って送信されたデータを受け取る
-    request_data = request.get_data()
-
-    # byte型のデータ"request_data"をstr型の文字列"request_data"にデコードを行う
-    charset = 'UTF-8'
-    request_text = request_data.decode(charset, 'replace')
-
-    # データを受け取ったかどうかを判定する
-    if len(request_text) != 0:
-        
-        # 文字列を" "および","で区分けする
-        data_list = re.split('[ ,]', request_text)
-
-        # 受け取ったデータをファイルに上書きで保存
-        with open(FILE_PATH, 'w', encoding='utf-8', newline="") as file:
-            writer = csv.writer(file)
-            writer.writerow(data_list)
-            file.close()
-
-    # データを取得
-    file = open(FILE_PATH)
-    open_data = file.read()
-    file.close()
-
-    # 値の送信
-    return open_data
-
+# '/robot_info/init'へのGETリクエストに対する操作
+@app.route('/robot_info/init', methods=["GET"])
+def init() -> None:
+    """ロボットの走行情報を初期化する."""
+    robot_info["state"] = "notReady"
+    try:
+        # ファイルを削除
+        os.remove(SKIP_FLAG_FILE)
+        print(f"Remove the file '{SKIP_FLAG_FILE}'.")
+    except OSError as e:
+        print(f"OS error: {e}")
 
 # '/robot_info/state'へのGETリクエストに対する操作
 @app.route('/robot_info/state', methods=["GET"])
-def send() -> str:
-    """GETリクエストに対しロボットの状況を返す.
-    "http://サーバIPアドレス:8000/robot_info/state"にGETリクエストされたときに実行される
+def getState() -> str:
+    """ロボットの状況を返す.
     
     Returns:
         robot_info["state"] (str):  サーバが持つロボットの状況
     """
-
-    # 値の送信
     return robot_info["state"]
-
 
 # '/robot_info/state'へのPOSTリクエストに対する操作
 @app.route('/robot_info/state', methods=["POST"])
-def update() -> str:
-    """POSTリクエストに対しロボットの状況を返す.
-    "http://サーバIPアドレス:8000/robot_info/state"にPOSTリクエストされたときに実行される
+def setState() -> str:
+    """ロボットの状況を設定する.
     
     Returns:
         robot_info["state"] (str):  サーバが持つロボットの状況
     """
-
     # request.get_data()を使って送信されたデータを受け取る
     request_data = request.get_data()
-
     # byte型のデータ"request_data"をstr型の文字列"request_data"にデコードを行う
     charset = 'UTF-8'
     request_text = request_data.decode(charset, 'replace')
-
     # ロボットの状況を更新する
     robot_info["state"] = request_text
+    return str(robot_info["state"])
 
-    # 値の送信
-    return robot_info["state"]
+# '/robot_info/skip_camera_action'へのPOSTリクエストに対する操作
+@app.route('/robot_info/skip_camera_action_true', methods=["POST"])
+def setTrueSkipCameraAction() -> None:
+    """撮影動作終了フラグを立てる."""
+    # C++側から確認できるようファイルを生成する(C++はファイルの有無で判断)
+    skip_camera_action_flag_file = pathlib.Path(SKIP_FLAG_FILE)
+    skip_camera_action_flag_file.touch()
+    try:
+        # 撮影終了フラグとして、空ファイルを作成する
+        with open(SKIP_FLAG_FILE, 'w') as file:
+            pass
+        print(f"Touch the file '{SKIP_FLAG_FILE}'.")
+    except IOError as e:
+        print(f"I/O error: {e}")
 
 
 # ポート番号の設定
 if __name__ == "__main__":
-
     ip = "127.0.0.1"
-    
     host = os.uname()[1]
     if host == "katlab":
         ip = "192.168.11.16"
