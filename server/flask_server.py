a0ce7ebe1add83c5f8d4bf90b5be9450632b221d
miyashita64 <javajavawookjavawook@gmail.com>
2023-09-27 17:04:33 +0900

    add: Webサーバにフィグ画像を提供するルートを追加

--- server/flask_server.py
+++ server/flask_server.py
@@ -6,7 +6,7 @@ import os
 import csv
 import re
 import pathlib
-from flask import Flask, request
+from flask import Flask, request, send_file
 
 # ロボットの走行情報
 robot_info = {
@@ -68,6 +68,24 @@ def setState() -> str:
     # ロボットの状況を更新する
     robot_info["state"] = request_text
     return robot_info["state"]
+
+# '/fig_image?file_name=フィグ画像名'へのGETリクエストに対する操作
+@app.route('/fig_image', methods=["GET"])
+def getFigImage():
+    """指定された名前のフィグ画像を返す.
+    
+    Returns:
+        フィグ画像
+    """
+    image_file_name = request.args.get("file_name")
+    image_file_path = f"../rear_camera_py/image_data/{image_file_name}"
+
+    # 画像送信
+    try:
+        return send_file(image_file_path, mimetype='image/png')
+    except FileNotFoundError:
+        # 送信失敗(ファイルがない場合)
+        return f"Not Found {image_file_path}", 404
 
 # '/robot_info/skip_camera_action_true'へのPOSTリクエストに対する操作
 @app.route('/robot_info/skip_camera_action_true', methods=["POST"])
